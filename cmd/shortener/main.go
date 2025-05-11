e59fe2ec1c8ddbbce2f354bbcfd3e7d90e303db0
Constantine <tmail99@gmail.com>
2025-05-11 13:09:48 +0300

    iter2

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -12,12 +12,7 @@ func main() {
 	storageInstance := storage.NewStorage()
 
 	mux.HandleFunc("POST /{$}", handlers.PostHandler(storageInstance))
-	//mux.HandleFunc("GET /{id}", handlers.GetHandler(storageInstance))
-
-	// Работает и так и так.
-	// Автотесты прошел! Верхний конечно логичнее.
-	// Пока оставлю и так и так, как упражнение
-	mux.HandleFunc("GET /{id}", storageInstance.GetHandler)
+	mux.HandleFunc("GET /{id}", handlers.GetHandler(storageInstance))
 
 	http.ListenAndServe(":8080", mux)
 }
--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -12,11 +12,11 @@ import (
 	"github.com/dr2cc/URLsShortener.git/internal/storage"
 )
 
-func generateShortURL(urlList *storage.URLStorage, longURL string) string {
+func generateAlias(us *storage.URLStorage, url string) string {
 
 	r := rand.New(rand.NewSource(time.Now().UnixNano()))
 
-	runes := []rune(longURL)
+	runes := []rune(url)
 	r.Shuffle(len(runes), func(i, j int) {
 		runes[i], runes[j] = runes[j], runes[i]
 	})
@@ -25,43 +25,56 @@ func generateShortURL(urlList *storage.URLStorage, longURL string) string {
 	//[:11] здесь сокращаю строку
 	id := reg.ReplaceAllString(string(runes[:11]), "")
 
-	storage.MakeEntry(urlList, id, longURL)
+	storage.MakeEntry(us, id, url)
 
 	return "/" + id
 }
 
 // Функция PostHandler уровня пакета handlers
-func PostHandler(ts *storage.URLStorage) http.HandlerFunc {
+func PostHandler(us *storage.URLStorage) http.HandlerFunc {
 	return func(w http.ResponseWriter, req *http.Request) {
-		param, err := io.ReadAll(req.Body)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusBadRequest)
-			return
+		switch req.Method {
+		case http.MethodPost:
+			param, err := io.ReadAll(req.Body)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusBadRequest)
+				return
+			}
+
+			// Преобразуем тело запроса (тип []byte) в строку:
+			url := string(param)
+			// Генерируем короткий идентификатор и создаем запись в нашем хранилище
+			alias := "http://" + req.Host + generateAlias(us, url)
+
+			// Устанавливаем статус ответа 201
+			w.WriteHeader(http.StatusCreated)
+			fmt.Fprint(w, alias)
+
+		default:
+			w.Header().Set("Location", "Method not allowed")
+			w.WriteHeader(http.StatusBadRequest)
 		}
-
-		// Преобразуем тело запроса (тип []byte) в строку:
-		longURL := string(param)
-		// Генерируем сокращённый URL и создаем запись в нашем хранилище
-		shortURL := "http://" + req.Host + generateShortURL(ts, longURL)
-
-		// Устанавливаем статус ответа 201
-		w.WriteHeader(http.StatusCreated)
-		fmt.Fprint(w, shortURL)
 	}
 }
 
 // Функция GetHandler уровня пакета handlers
-func GetHandler(ts *storage.URLStorage) http.HandlerFunc {
+func GetHandler(us *storage.URLStorage) http.HandlerFunc {
 	return func(w http.ResponseWriter, req *http.Request) {
-		id := strings.TrimPrefix(req.RequestURI, "/")
-		longURL, err := storage.GetEntry(ts, id)
-		if err != nil {
-			w.Header().Set("Location", err.Error())
-			w.WriteHeader(http.StatusBadRequest)
-			return
-		}
+		switch req.Method {
+		case http.MethodGet:
+			id := strings.TrimPrefix(req.RequestURI, "/")
+			url, err := storage.GetEntry(us, id)
+			if err != nil {
+				w.Header().Set("Location", err.Error())
+				w.WriteHeader(http.StatusBadRequest)
+				return
+			}
 
-		w.Header().Set("Location", longURL)
-		w.WriteHeader(http.StatusTemporaryRedirect)
+			w.Header().Set("Location", url)
+			w.WriteHeader(http.StatusTemporaryRedirect)
+		default:
+			w.Header().Set("Location", "Method not allowed")
+			w.WriteHeader(http.StatusBadRequest)
+		}
 	}
 }
