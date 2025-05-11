bd82ce50f52e3a47d05f304df5fc86e1786914f8
Constantine <tmail99@gmail.com>
2025-05-11 12:56:08 +0300

    iter1

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,23 @@
 package main
 
-func main() {}
+import (
+	"net/http"
+
+	"github.com/dr2cc/URLsShortener.git/internal/handlers"
+	"github.com/dr2cc/URLsShortener.git/internal/storage"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	storageInstance := storage.NewStorage()
+
+	mux.HandleFunc("POST /{$}", handlers.PostHandler(storageInstance))
+	//mux.HandleFunc("GET /{id}", handlers.GetHandler(storageInstance))
+
+	// Работает и так и так.
+	// Автотесты прошел! Верхний конечно логичнее.
+	// Пока оставлю и так и так, как упражнение
+	mux.HandleFunc("GET /{id}", storageInstance.GetHandler)
+
+	http.ListenAndServe(":8080", mux)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/dr2cc/URLsShortener.git
+
+go 1.23.8
