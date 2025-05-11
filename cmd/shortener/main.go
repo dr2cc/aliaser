e19fb4774a0ab57505315303cf52fb8c3c87019e
Constantine <tmail99@gmail.com>
2025-05-11 14:49:31 +0300

    iter3

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,18 +1,22 @@
 package main
 
 import (
-	"net/http"
-
 	"github.com/dr2cc/URLsShortener.git/internal/handlers"
 	"github.com/dr2cc/URLsShortener.git/internal/storage"
+	"github.com/go-chi/chi"
 )
 
 func main() {
-	mux := http.NewServeMux()
+	//mux := http.NewServeMux()
+	mux := chi.NewRouter()
+
 	storageInstance := storage.NewStorage()
 
-	mux.HandleFunc("POST /{$}", handlers.PostHandler(storageInstance))
-	mux.HandleFunc("GET /{id}", handlers.GetHandler(storageInstance))
+	// // Обработчик net/http
+	//mux.HandleFunc("POST /{$}", handlers.PostHandler(storageInstance))
+	//mux.HandleFunc("GET /{id}", handlers.GetHandler(storageInstance))
 
-	http.ListenAndServe(":8080", mux)
+	// Обработчик chi
+	mux.Post("/", handlers.PostHandler(storageInstance))
+	mux.Get("/{id}", handlers.GetHandler(storageInstance))
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/dr2cc/URLsShortener.git
 
 go 1.23.8
+
+require github.com/go-chi/chi v1.5.5
